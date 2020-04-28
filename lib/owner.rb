9155a57c91a5c1c143ee688aa6a554f1126133e9
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 03:29:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats]length} cat(s)."
+    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 
 
